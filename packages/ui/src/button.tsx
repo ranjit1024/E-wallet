d2db9d355bec0bbef0e0809eaa37627530b4770c
ranjit <ranjitdas2048@gmail.com>
2025-04-01 11:48:13 +0530

    done with tailwind

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,20 +1,9 @@
 "use client";
 
-import { ReactNode } from "react";
 
-interface ButtonProps {
-  children: ReactNode;
-  className?: string;
-  appName: string;
-}
 
-export const Button = ({ children, className, appName }: ButtonProps) => {
+export const Button = () => {
   return (
-    <button
-      className={className}
-      onClick={() => alert(`Hello from your ${appName} app!`)}
-    >
-      {children}
-    </button>
+    <div className="bg-lime-200">ranjit</div>
   );
 };
