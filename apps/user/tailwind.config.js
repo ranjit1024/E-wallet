7bee53ca15547e5836b23b748089b9e7e33d3ea8
ranjit <ranjitdas2048@gmail.com>
2025-04-02 22:40:00 +0530

    databas added

--- apps/user/tailwind.config.js
+++ apps/user/tailwind.config.js
@@ -8,7 +8,7 @@ export default {
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
     "../../packages/ui/**/*.{js,ts,jsx,tsx,mdx}"
-  ],
+  ], 
   theme: {
     extend: {},
   },
