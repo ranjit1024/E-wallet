96cb28d02dad6cf53dcc8c963f0b3357452048ed
ranjit <ranjitdas2048@gmail.com>
2025-05-26 08:51:48 +0530

    added

--- apps/user/app/page.tsx
+++ apps/user/app/page.tsx
@@ -35,24 +35,24 @@ export default function Home() {
           duration: 0.5,
           ease: "easeIn",
         }}
-        className="flex flex-col  justify-center relative  items-center mx-20 mt-[7%] p-4 text-[5.5vw] font-semibold max-md:w-[100vw] max-md:mx-1 max-md:text-center max-md:text-3xl  text-center tracking-normal mb-5"
+        className="flex flex-col max-md:h-[45vh]  justify-center relative  items-center mx-20 mt-[7%] p-4 max-md:p-1 text-[5.5vw] font-semibold max-md:w-[100vw] max-md:mx-1 max-md:text-center max-md:text-3xl  text-center tracking-normal max-md:pb-0"
       >
-        <div className="text-center w-[90vw] ">
+        <div className="text-center w-[100%] max-md:text-4xl ">
           {/* Safeguard your{" "}
           <*/}
           Secure your money with{" "}
           <span className="relative w-[100%]">
-            <div className="p-4 absolute  bg-blue-400 opacity-20 left-0 bottom-4 w-[96%] -z-10"></div>
+            <div className="p-4 absolute  bg-blue-400 opacity-20 max-md:bottom-1  left-0 bottom-4 w-[96%] -z-10"></div>
             trusted{" "}
           </span>{" "}
         </div>
 
-        <div className="text-center w-[80%] relative -mt-5 max-md:mt-0">
+        <div className="text-center w-[80%] max-md:w-[100%] relative -mt-5 max-md:mt-0 max-md:text-4xl">
           bank-level protection
           <div className="h-1 w-1 absolute top-12 opacity-80  transition-all bg-yellow-400 p-1 rounded-full"></div>
         </div>
 
-        <div className="absolute top-10 -left-10 -rotate-90 opacity-40 max-sm:hidden  transition-all">
+        <div className="absolute top-10  -left-10 -rotate-90 opacity-40 max-sm:hidden  transition-all">
           <Image
             width="40"
             height="40"
@@ -84,7 +84,7 @@ export default function Home() {
           duration: 0.5,
           delay: 0.2,
         }}
-        className="z-190 text-center flex items-center justify-center w-[100%]  -mt-"
+        className="z-190 text-center flex items-center justify-center w-[100%] max-md:w-[100vw] max-md:p-2  "
       >
         <p className="w-[75%] text-lg text-gray-700 mb-5 max-md:w-[100vw]">
           Paytm is a secure and reliable platform for transferring money between
@@ -106,7 +106,7 @@ export default function Home() {
           ease: "easeOut",
           delay: 0.4,
         }}
-        className="z-190 relative  text-center flex items-center  justify-center w-[100%] mt-10 gap-5"
+        className="z-190 relative  text-center flex items-center  justify-center w-[100%] mt-10 gap-5 max-md:mt-1"
       >
         <div>
           <motion.button
@@ -129,10 +129,10 @@ export default function Home() {
         <div className="">
           <Image className="" src="/side.png" alt="" width={130} height={20} />
         </div>
-        <div className="absolute opacity-50 right-[20%] -top-4">
+        <div className="absolute opacity-50 right-[20%] max-md:hidden -top-4">
           <Image src="/side2.png" alt="" width={45} height={48} />
         </div>
-        <div className="absolute opacity-40 left-[20%] top-3">
+        <div className="absolute opacity-40 left-[20%] max-md:left-0 max-md:top-2 top-3">
           <Image
             width="40"
             height="50"
@@ -146,7 +146,7 @@ export default function Home() {
         initial={{ y: -10, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
         transition={{ duration: 1, delay: 1 }}
-        className=" mt-10 flex justify-center w-[100%] max-md:hidden items-center  gap-12 px-[12%] pt-20"
+        className=" mt-10 flex justify-center w-[100%] max-md:flex-wrap max-md:px-5 items-center  gap-12 px-[12%] pt-20"
       >
         <div className="size-[90%]  px-2 bg-[#fff5e4]  rounded-xl">
           <Image src="/test.png" alt="" height={500} width={500} />
@@ -204,7 +204,7 @@ export default function Home() {
         </p>
       </div>
 
-      <div className="all cards grid grid-cols-3  max-sm:flex max-md:flex-wrap px-40 mt-12 gap-10 max-md:px-1  max-md:w-[100vw]  ">
+      <div className="all cards grid grid-cols-3  max-sm:flex max-md:flex-wrap px-40 mt-12 gap-10 max-md:px-5 max-md:gap-5  max-md:w-[100vw]  ">
         <motion.div
           initial={{ opacity: 0, y: 50, scale: 0.95 }}
           whileInView={{ opacity: 1, y: 0, scale: 1 }}
@@ -315,7 +315,7 @@ export default function Home() {
       </div>
 
       <div className="mt-[10%]">
-        <div className="px-[11%] flex gap-10 max-md:flex-wrap max-md:px-1   max-md:w-[100vw] max-md:justify-center ">
+        <div className="px-[11%] flex gap-10 max-md:flex-wrap max-md:px-5   max-md:w-[100vw] max-md:justify-center ">
           <motion.div
             initial={{ opacity: 0, x: -20 }}
             whileInView={{ opacity: 1, x: 0 }}
@@ -359,7 +359,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="mt-[10%] mx-[11%] flex gap-10 max-sm:flex-wrap">
+      <div className="mt-[10%] mx-[11%] max-md:mx-2 flex gap-10 max-sm:flex-wrap">
         <motion.div
           initial={{ opacity: 0, x: -20 }}
           whileInView={{ opacity: 1, x: 0 }}
@@ -415,14 +415,14 @@ export default function Home() {
           whileInView={{ opacity: 1, x: 0 }}
           transition={{ duration: 1, ease: "easeInOut" }}
           viewport={{ once: true, amount: 0.2 }}
-          className="w-[50%] p-1 bg-gray-100 rounded-lg max-md:hidden"
+          className="w-[50%] p-1 bg-gray-100 rounded-lg max-md:hidden "
         >
           <Image src="/fourth.png" alt="" height={500} width={500} />
         </motion.div>
       </div>
 
       <div className="footer mt-[13%] mx-2">
-        <footer className="flex flex-row fitems-center justify-center w-full py-6 text-center border-t gap-y-6 gap-x-12 border-slate-200 md:justify-between">
+        <footer className="flex flex-row  max-md:flex  max-md:flex-wrap fitems-center justify-center w-full py-6 text-center border-t gap-y-6 gap-x-12 border-slate-200 md:justify-between">
           <div className="block text-slate-800 font-semibold text-sm px-10">
             <Logo />
           </div>
--- apps/user/link.ts
+++ apps/user/link.ts
@@ -1,1 +1,2 @@
-export const prod = "https://ewallet.10xdev.shop"
+// export const prod = "https://ewallet.10xdev.shop"
+export const prod = "http://localhost:3000"
